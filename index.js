2d1f7bc20eb5296f370b195c3d274e257219b0eb
rajan <rajanpatel36816145@gail.com>
2022-09-03 01:47:19 +0530

    new site

---
+++ index.js
@@ -0,0 +1,13 @@
+//  type writing effect------------------------------------------
+
+
+ var typingEffect = new Typed(".multitext",{
+    strings : ["Frontend Devloper","Photographer","Photo Designer"],
+    loop : true,
+    typeSpeed : 80,
+    backSpeed : 5,
+    startDelay : 100,
+    backDelay : 100
+    
+    
+    });
